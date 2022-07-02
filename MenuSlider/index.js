a8649d5ff809fba5ea3d5bc6894b2b672541f8ce
ronwain <100219248+ronwain@users.noreply.github.com>
2022-07-02 22:01:20 +0200

    Form Validator JS check length, email a password match

--- MenuSlider/index.js
+++ MenuSlider/index.js
@@ -20,13 +20,15 @@ function showSuccess(input) {
 }
 
 //Check email valid
-const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not Valid");
+  }
+}
 
 //CHcec required fields
 function checkRequired(inputArr) {
@@ -39,6 +41,30 @@ function checkRequired(inputArr) {
   });
 }
 
+//Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFiledName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFiledName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+//Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Password doesnt match");
+  }
+}
+
 //Get fieldname
 function getFiledName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -49,6 +75,10 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 5, 15);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 
   console.log(username.value);
 });
